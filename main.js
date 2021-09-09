94fc4752d03c716bd73c155bd550ae5dd4c1ac99
Pandademic <stewie.gupte@gmail.com>
2021-09-09 19:57:36 -0400

    everything SHOULD work now

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function setup(){
    video=createCapture(VIDEO);
    video.size(550,500);
-   //canvas=createCanvas(450,450);
-   //canvas.position(560,150);
+   canvas=createCanvas(350,350);
+   canvas.position(560,150);
    poseNet=ml5.poseNet(video,modelLoaded);
    poseNet.on('pose',gotPoses); 
 }
