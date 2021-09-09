f6c307312fcba7612f895ae4f546ff98287bcc73
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-09-09 19:50:07 -0400

    lower video size

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function setup(){
    video=createCapture(VIDEO);
-   video.size(550,500);
+   video.size(150,100);
    canvas=createCanvas(150,150);
    canvas.position(560,150);
    poseNet=ml5.poseNet(video,modelLoaded);
