f5788c9e3c2b9c22c369ad36be37c02ec73d562d
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-09-09 19:49:07 -0400

    too small canvas

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function setup(){
    video=createCapture(VIDEO);
    video.size(550,500);
-   canvas=createCanvas(450,450);
+   canvas=createCanvas(150,150);
    canvas.position(560,150);
    poseNet=ml5.poseNet(video,modelLoaded);
    poseNet.on('pose',gotPoses); 
