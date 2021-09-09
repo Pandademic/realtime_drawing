bde8d09dde28c773ebbf7629bf55c865a2ab3ada
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-09-09 19:46:38 -0400

    reduce canvas again

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function setup(){
    video=createCapture(VIDEO);
    video.size(550,500);
-   canvas=createCanvas(500,550);
+   canvas=createCanvas(450,450);
    canvas.position(560,150);
    poseNet=ml5.poseNet(video,modelLoaded);
    poseNet.on('pose',gotPoses); 
