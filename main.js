ceaa2955ccde8714b7dbd4fdcb226a20e47a7669
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-10-07 19:14:03 -0400

    sometjing

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+noseX=0;
+noseY=0;
+
 function setup(){
    video=createCapture(VIDEO);
    video.size(550,500);
@@ -7,7 +10,10 @@ function setup(){
    poseNet.on('pose',gotPoses); 
 }
 function draw(){
-   background('#1c79c0');
+    background('#1c79c0');
+    fill('#ffd900');
+    stroke('#ffd900');
+    square(noseX,noseY,100);
 
 }
 function modelLoaded(){
@@ -16,6 +22,11 @@ function modelLoaded(){
 function gotPoses(){
     if(results.length>0){
         console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX="+noseX+" noseY="+noseY);
+
+        
     }
     else {
         console.error('There are no results');
