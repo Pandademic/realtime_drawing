6ce2da6f0c82cee2451a85784b096fef23601b71
Pandademic <stewie.gupte@gmail.com>
2021-10-07 19:28:46 -0400

    v3

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 noseX=0;
 noseY=0;
+LWX=0;  //LeftWristX
+RWX=0;
+diff=0;
 
 function setup(){
    video=createCapture(VIDEO);
@@ -11,9 +14,10 @@ function setup(){
 }
 function draw(){
     background('#1c79c0');
+    document.getElementById("square_size").innerHTML="Width &  Height are currently:"+diff+"px";
     fill('#ffd900');
     stroke('#ffd900');
-    square(noseX,noseY,100);
+    square(noseX,noseY,diff);
 
 }
 function modelLoaded(){
@@ -25,8 +29,11 @@ function gotPoses(results){
         noseX=results[0].pose.nose.x;
         noseY=results[0].pose.nose.y;
         console.log("noseX="+noseX+" noseY="+noseY);
-
-        
+ 
+        LWX=results[0].pose.leftWrist.x;
+        RWX=results[0].pose.rightWrist.x;
+        diff=floor(LWX-RWX)
+        console.log("rwx:"+RWX+" Lwx:"+LWX+" diffrence:"+diff);
     }
     else {
         console.error('There are no results');
